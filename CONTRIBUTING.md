31973e10c1e8d289ade4c6d6b2579c3fbeb6f748
Craig Mulligan <hobochildster@gmail.com>
2018-01-09 23:18:00 -0800

    feat(prefetch): Allows use of custom link component that prefetches data as well as js route

---
+++ CONTRIBUTING.md
@@ -0,0 +1,20 @@
+## Symlinking in development
+
+During development a plain symlink won't work because next.js router requires a single instance. Therefore we have to resolve to a single next module. To do this follow the steps:
+
+make next-static-tools linkable
+```
+yarn link
+```
+
+link them to the next-site you'd like to test (from the site in question)
+```
+yarn link next-static-tools
+```
+
+The we export a path used by babel to setup some aliases
+```
+export NEXT_STATIC_TOOLS_TEST_REPO=path/to/test/site
+```
+
+Then do your thang!
---
+++ preset.js
@@ -0,0 +1,19 @@
+// Because next/router can only have one instance there are times when you may want to test the site 'next-static-tools' symlinked. In these cases you can configure TEST_REPO to the absolute path of the repo you've symlinked with and this will handle resolve to the correct next.js
+const isDev = process.env.NEXT_STATIC_TOOLS_TEST_REPO ? true : false
+const TEST_REPO = process.env.NEXT_STATIC_TOOLS_TEST_REPO
+const relativeResolve = p => require.resolve(`${TEST_REPO}/node_modules/next/${p}`)
+
+const devConfig = {
+  plugins: [[
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        alias: {
+          'next/link': relativeResolve('link'),
+          'next/head': relativeResolve('head'),
+          'next/router': relativeResolve('router')
+        }
+      }
+  ]]
+}
+
+module.exports = isDev ? devConfig : {} 
