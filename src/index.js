fbf5b5e5e52f2f5b2f8670812fa42270bb974855
Craig Mulligan <hobochildster@gmail.com>
2018-01-12 13:38:33 +0000

    feat(config): Make options (port, endpoint outdir) configureable

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { parse } = require('url')
 const { GraphQLServer } = require('graphql-yoga')
+const swPrecache = require('sw-precache')
 
 const middleware = (handle, options) => (req, res, next) => {
   const parsedUrl = parse(req.url, true)
@@ -10,16 +11,42 @@ const middleware = (handle, options) => (req, res, next) => {
     pathname.startsWith(options.endpoint)
   ) {
     next()
+  } else if (pathname === '__NEXT_STATIC_TOOLS__') {
+    res.send(options)
   } else {
     handle(req, res, parsedUrl)
   }
 }
 
-export default ({ typeDefs, resolvers, app, options }) => {
+const writeServiceWorker = rootDir => {
+  return new Promise((resolve, reject) => {
+    swPrecache.write(
+      `${rootDir}/service-worker.js`,
+      {
+        stripPrefix: rootDir,
+        runtimeCaching: [
+          {
+            urlPattern: /(.*)/,
+            handler: 'networkFirst'
+          }
+        ]
+      },
+      (err, data) => {
+        if (err) {
+          reject(err)
+        }
+        resolve(data)
+      }
+    )
+  })
+}
+
+export default async ({ typeDefs, resolvers, app, options }) => {
   const defaults = {
     playground: '/playground',
     endpoint: '/graphql',
-    port: 4000
+    port: 5000,
+    outdir: './out'
   }
 
   options = {
@@ -27,7 +54,10 @@ export default ({ typeDefs, resolvers, app, options }) => {
     ...options
   }
 
+  process.env.__NEXT_STATIC_TOOLS__ = JSON.stringify(options)
+
   const server = new GraphQLServer({ typeDefs, resolvers })
+  await writeServiceWorker(options.outdir)
   server.express.use(middleware(app.getRequestHandler(), options))
   server.start(options, () =>
     // eslint-disable-next-line no-console
